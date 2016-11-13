6a5c2c0c3a4a4a18b1e1d81c01f66ad29d6e15bc
Constantine Lutas <c.lutas@icloud.com>
2016-11-13 12:43:31 +0200

    Add sample login screen for Trakt.tv

---
+++ Series/TraktLoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TraktLoginViewController.swift
+//  Series
+//
+//  Created by Konstantinos Loutas on 11/13/16.
+//  Copyright © 2016 Constantine Lutas. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class TraktLoginViewController: UIViewController {
+
+    let trakt = TraktClient()
+    
+    @IBOutlet weak var pinTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        presentAuthenticationView()
+    }
+    
+    @IBAction func submitPinButtonPressed(_ sender: UIButton) {
+        print("Submitted PIN: \(pinTextField.text)")
+    }
+    
+    func presentAuthenticationView() {
+        if let authenticationUrl = trakt.authenticationRequestUrl {
+            print(authenticationUrl)
+            let sVC = SFSafariViewController(url: authenticationUrl)
+            self.present(sVC, animated: true, completion: nil)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
