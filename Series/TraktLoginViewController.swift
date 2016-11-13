ed247ec9902800c5c88fdf8af74f90d50ac450c7
Constantine Lutas <c.lutas@icloud.com>
2016-11-13 19:22:13 +0200

    Refactor old comments

--- Series/TraktLoginViewController.swift
+++ Series/TraktLoginViewController.swift
@@ -44,16 +44,5 @@ class TraktLoginViewController: UIViewController {
             self.present(sVC, animated: true, completion: nil)
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
