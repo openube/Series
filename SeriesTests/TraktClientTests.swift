22b7313de2b5af9c4569372779453571464f6b6b
Constantine Lutas <c.lutas@icloud.com>
2016-11-13 12:44:23 +0200

    Add testAuthorizeApplication() test

--- SeriesTests/TraktClientTests.swift
+++ SeriesTests/TraktClientTests.swift
@@ -7,29 +7,33 @@
 //
 
 import XCTest
+@testable import Series
 
 class TraktClientTests: XCTestCase {
+    
+    var trakt: TraktClient!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        trakt = TraktClient()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
+    func testAuthorizeApplication() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        
+        let expect = expectation(description: "Response of GET request received")
+        var result = false
+        trakt.authorizeApplication { (success, string) in
+            result = success
+            print(string!)
+            expect.fulfill()
         }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssert(result, "Authorization not successful")
     }
     
 }
