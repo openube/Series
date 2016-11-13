d02fb4445f915657a804478b702b3630c66b7205
Constantine Lutas <c.lutas@icloud.com>
2016-11-14 00:12:33 +0200

    Add console logging for Trakt.tv user access token

--- Series/AppDelegate.swift
+++ Series/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let defaults = UserDefaults.standard
+        if let userAccessToken = defaults.object(forKey: "accessToken") {
+            print("User access token: \(userAccessToken)")
+        }
         return true
     }
     
